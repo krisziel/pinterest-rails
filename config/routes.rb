d290df74e0c5e8e4dd31b19b5ac3686ba5d2436f
krisziel <krisziel@mac.com>
2015-01-20 21:24:37 -0800

    formatted responses for jsonp

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'profiles#index'
+  get ':username' => 'profiles#show', :format => 'json'
+  get ':username/:board' => 'profiles#board', :format => 'json'
   get ':username.json' => 'profiles#show', :format => 'json'
   get ':username/:board.json' => 'profiles#board', :format => 'json'
 end
