49df3c12e7c53d60ed9855892074b08105c04760
krisziel <krisziel@mac.com>
2015-01-29 08:52:19 -0800

    cross domain json or jsonp support

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,16 +1,17 @@
 class ProfilesController < ApplicationController
 
   protect_from_forgery except: [:profile,:board]
-  def index
-    # @data = PinterestApi::Profile.new('designror')
-  end
 
   def profile
+    headers['Access-Control-Allow-Origin'] = '*' if !params[:callback]
     @data = PinterestApi::Profile.new(params[:username])
+    render :json => @data, :callback => params[:callback]
   end
 
   def board
+    headers['Access-Control-Allow-Origin'] = '*' if !params[:callback]
     @data = PinterestApi::Board.new(params[:username],params[:board])
+    render :json => @data, :callback => params[:callback]
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'profiles#index'
-  get ':username' => 'profiles#profile', :format => 'js'
-  get ':username/:board' => 'profiles#board', :format => 'js'
-  get ':username.json' => 'profiles#profile', :format => 'js'
-  get ':username/:board.json' => 'profiles#board', :format => 'js'
+  get ':username' => 'profiles#profile'
+  get ':username/:board' => 'profiles#board'
+  get ':username.json' => 'profiles#profile'
+  get ':username/:board.json' => 'profiles#board'
 end
